c55097d089edd30793e1ff1f216d86c6c6f512e1
Nikolai Sison <sisonnikolai@gmail.com>
2020-10-25 21:32:56 +0800

    Initialized database table creation for list of books

---
+++ BookListRazor/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Models
+{
+    public class Book
+    {
+        [Key] // automatically adds ID as an identity column
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
